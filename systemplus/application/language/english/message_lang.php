b01b8c75e09b8f868328e138ec4849b98fa23d5f
sourav <sourav.dara@netwininfo.com>
2018-06-11 14:21:06 +0530

    THis is language file

---
+++ systemplus/application/language/english/message_lang.php
@@ -0,0 +1,18 @@
+<?php
+	$lang['please_enter_dynamic'] = 'Please Enter **field**';
+	$lang['csrf_error_message'] = 'Invalid login form is submitted. Please try again.';
+	$lang['add_success_message'] = '**module** Added Successfully';
+	$lang['edit_success_message'] = '**module** Edited Successfully';
+	$lang['delete_success_message'] = '**module** Deleted Successfully';
+	$lang['active_confirmation'] = 'Do you want to active this **module** ?';
+	$lang['inactive_confirmation'] = 'Do you want to inactive this **module** ?';
+	$lang['valid_data_error_msg'] = 'Please enter valid data.';
+	$lang['exact_image_size'] = 'Image size should be exact **width** X **height** pixel';
+	$lang['image_type_error_msg'] = 'Please upload JPG|PNG|JPEG image.';
+	$lang['required_upload_image'] = 'Please upload image';
+	$lang['delete_confirmation'] = 'Are you sure to delete ?';
+	$lang['minimum_image_dimension'] = 'Image dimension should be greater or equal to **width** X **height** pixel';
+	$lang['please_select_dynamic'] = 'Please Select **field**';
+	$lang['geocode_invalid_address'] = 'Invalid address as per geocode';
+
+?>
